5042b831aa7ab6af6444e075e8181ed429c54c75
pedrooayres <pedrohayres@yahoo.com.br>
2025-05-23 12:52:38 +0000

    demais funções questões 10 até a 14

--- estrutura de dados/atividade2.c
+++ estrutura de dados/atividade2.c
@@ -32,3 +32,65 @@ void inserirFinal(No* tabela[], int valor){
         aux -> prox = novo;
     }
 }
+// imprimir lista(10º questão)
+void imprimir(No* tabela[]){
+    for(int i = 0; i < TAM; i++){
+        printf("%d: ", i);
+        No* aux = tabela[i];
+        while(aux != NULL){
+            printf("%d ", aux -> valor);
+            aux = aux -> prox;
+        }
+        printf("\n");
+    }
+}
+// verificar chave(11º questão)
+int chave_existe(No* tabela[], int valor){
+    int indice = valor % TAM;
+    No* aux = tabela[indice];
+    while(aux != NULL){
+        if(aux -> valor == valor){
+            return 1;
+        }
+        aux = aux -> prox;
+    }
+    return 0;
+}
+// taxa de ocupação(12º questão)
+float taxa_ocupacao(No* tabela[]){
+    int oucupadas = 0;
+    for(int i = 0; i < TAM; i++){
+        No* aux = tabela[i];
+        while(aux != NULL){
+            oucupadas++;
+            aux = aux -> prox;
+        }
+    }
+    return (float) oucupadas / TAM;
+}
+// oucupadasar colisões (13º questão)
+int contar_colisoes(No* tabela[]){
+    int colisoes = 0;
+    for(int i = 0; i < TAM; i++){
+        No* aux = tabela[i];
+        while(aux != NULL){
+            if(aux -> prox != NULL){
+                colisoes++;
+            }
+            aux = aux -> prox;
+        }
+    }
+    return colisoes;
+}
+// contar total de elementos(14º questão)
+int contar_elementos(No* tabela[]){
+    int elementos = 0;
+    for(int i = 0; i < TAM; i++){
+        No* aux = tabela[i];
+        while(aux != NULL){
+            elementos++;
+            aux = aux -> prox;
+        }
+    }
+    return elementos;
+}
